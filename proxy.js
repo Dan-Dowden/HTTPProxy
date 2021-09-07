ead0efbf8cb38e5c5242df49dff08f58e27e282f
Dan <dan.dowden@outlook.com>
2021-09-07 22:24:18 +1200

    pass on authorizations headers

--- proxy.js
+++ proxy.js
@@ -1,19 +1,28 @@
 const express = require("express");
+var cors = require("cors");
 const https = require("https");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 const port = 4000;
+const apiUrl = process.env.API_URL;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
+app.options("*", cors());
+
 app.all("/*", (req, res) => {
   if (req.method === "GET") {
     var options = {
       hostname: apiUrl,
       path: "/prod" + req.originalUrl,
-      method: "GET"
+      method: "GET",
+      headers: {
+        Authorization: req.headers.authorization,
+      },
     };
 
     var apiReq = https.request(options, function (apiRes) {
